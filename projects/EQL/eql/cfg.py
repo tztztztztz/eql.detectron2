d24f9826b256809afbb175287bf64fd6f0da8442
tanjingru <tanjingru120@gmail.com>
2020-03-13 01:58:41 +0800

    remove some redundant code to make the implementation more clear

--- projects/EQL/eql/cfg.py
+++ projects/EQL/eql/cfg.py
@@ -3,4 +3,7 @@ def add_eql_config(cfg):
     Add config for EQL.
     """
     cfg.MODEL.ROI_HEADS.LAMBDA = 0.00177
+    cfg.MODEL.ROI_HEADS.PRIOR_PROB = 0.001
 
+    # legacy cfg key (make model compatible with previous ckpt)
+    cfg.MODEL.ROI_HEADS.FREQ_INFO = ""
--- projects/EQL/eql/fast_rcnn.py
+++ projects/EQL/eql/fast_rcnn.py
@@ -2,13 +2,9 @@ import logging
 import math
 import numpy as np
 import torch
-from fvcore.nn import smooth_l1_loss
 from torch import nn
 from torch.nn import functional as F
-from detectron2.layers import cat
-from detectron2.utils.events import get_event_storage
-from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputLayers, FastRCNNOutputs, \
-    fast_rcnn_inference
+from detectron2.modeling.roi_heads.fast_rcnn import FastRCNNOutputLayers, FastRCNNOutputs
 
 logger = logging.getLogger(__name__)
 
@@ -20,7 +16,7 @@ class EQLFastRCNNOutputLayers(FastRCNNOutputLayers):
       (2) classification scores
     """
 
-    def __init__(self, input_size, num_classes, cls_agnostic_bbox_reg, box_dim=4):
+    def __init__(self, input_size, num_classes, cls_agnostic_bbox_reg, box_dim=4, prior_prob=0.001):
         """
         Args:
             input_size (int): channels, or (channels, height, width)
@@ -42,20 +38,11 @@ class EQLFastRCNNOutputLayers(FastRCNNOutputLayers):
 
         nn.init.normal_(self.cls_score.weight, std=0.01)
         nn.init.normal_(self.bbox_pred.weight, std=0.001)
-
         nn.init.constant_(self.bbox_pred.bias, 0)
 
-        prior_prob = 0.001 # roughly 1 / num_clasess (1231)
         bias_value = -math.log((1 - prior_prob) / prior_prob)
-        torch.nn.init.constant_(self.cls_score.bias, bias_value)
-
-    def forward(self, x):
-        if x.dim() > 2:
-            x = torch.flatten(x, start_dim=1)
-        scores = self.cls_score(x)
-        proposal_deltas = self.bbox_pred(x)
-        return scores, proposal_deltas
-
+        nn.init.constant_(self.cls_score.bias, bias_value)
+        
 
 class EQLFastRCNNOutputs(FastRCNNOutputs):
     """
@@ -88,48 +75,14 @@ class EQLFastRCNNOutputs(FastRCNNOutputs):
                 the smooth L1 loss function. When set to 0, the loss becomes L1. When
                 set to +inf, the loss becomes constant 0.
         """
-        self.box2box_transform = box2box_transform
-        self.num_preds_per_image = [len(p) for p in proposals]
-        self.pred_class_logits = pred_class_logits
-        self.pred_proposal_deltas = pred_proposal_deltas
-        self.smooth_l1_beta = smooth_l1_beta
+        super(EQLFastRCNNOutputs, self).__init__(box2box_transform=box2box_transform,
+                                                 pred_class_logits=pred_class_logits,
+                                                 pred_proposal_deltas=pred_proposal_deltas,
+                                                 proposals=proposals,
+                                                 smooth_l1_beta=smooth_l1_beta)
+
         self.lambda_ = lambda_
         self.freq_info = freq_info
-
-        box_type = type(proposals[0].proposal_boxes)
-        # cat(..., dim=0) concatenates over all images in the batch
-        self.proposals = box_type.cat([p.proposal_boxes for p in proposals])
-        assert not self.proposals.tensor.requires_grad, "Proposals should not require gradients!"
-        self.image_shapes = [x.image_size for x in proposals]
-
-        # The following fields should exist only when training.
-        if proposals[0].has("gt_boxes"):
-            self.gt_boxes = box_type.cat([p.gt_boxes for p in proposals])
-            assert proposals[0].has("gt_classes")
-            self.gt_classes = cat([p.gt_classes for p in proposals], dim=0)
-
-    def _log_accuracy(self):
-        """
-        Log the accuracy metrics to EventStorage.
-        """
-        num_instances = self.gt_classes.numel()
-        pred_classes = self.pred_class_logits.argmax(dim=1)
-        bg_class_ind = self.pred_class_logits.shape[1] - 1
-
-        fg_inds = (self.gt_classes >= 0) & (self.gt_classes < bg_class_ind)
-        num_fg = fg_inds.nonzero().numel()
-        fg_gt_classes = self.gt_classes[fg_inds]
-        fg_pred_classes = pred_classes[fg_inds]
-
-        num_false_negative = (fg_pred_classes == bg_class_ind).nonzero().numel()
-        num_accurate = (pred_classes == self.gt_classes).nonzero().numel()
-        fg_num_accurate = (fg_pred_classes == fg_gt_classes).nonzero().numel()
-
-        storage = get_event_storage()
-        storage.put_scalar("fast_rcnn/cls_accuracy", num_accurate / num_instances)
-        if num_fg > 0:
-            storage.put_scalar("fast_rcnn/fg_cls_accuracy", fg_num_accurate / num_fg)
-            storage.put_scalar("fast_rcnn/false_negative", num_false_negative / num_fg)
 
     def exclude_func(self):
         # instance-level weight
@@ -163,78 +116,6 @@ class EQLFastRCNNOutputs(FastRCNNOutputs):
 
         return torch.sum(cls_loss * eql_w) / self.n_i
 
-    def smooth_l1_loss(self):
-        """
-        Compute the smooth L1 loss for box regression.
-
-        Returns:
-            scalar Tensor
-        """
-        gt_proposal_deltas = self.box2box_transform.get_deltas(
-            self.proposals.tensor, self.gt_boxes.tensor
-        )
-        box_dim = gt_proposal_deltas.size(1)  # 4 or 5
-        cls_agnostic_bbox_reg = self.pred_proposal_deltas.size(1) == box_dim
-        device = self.pred_proposal_deltas.device
-
-        bg_class_ind = self.pred_class_logits.shape[1] - 1
-
-        # Box delta loss is only computed between the prediction for the gt class k
-        # (if 0 <= k < bg_class_ind) and the target; there is no loss defined on predictions
-        # for non-gt classes and background.
-        # Empty fg_inds produces a valid loss of zero as long as the size_average
-        # arg to smooth_l1_loss is False (otherwise it uses torch.mean internally
-        # and would produce a nan loss).
-        fg_inds = torch.nonzero((self.gt_classes >= 0) & (self.gt_classes < bg_class_ind)).squeeze(
-            1
-        )
-        if cls_agnostic_bbox_reg:
-            # pred_proposal_deltas only corresponds to foreground class for agnostic
-            gt_class_cols = torch.arange(box_dim, device=device)
-        else:
-            fg_gt_classes = self.gt_classes[fg_inds]
-            # pred_proposal_deltas for class k are located in columns [b * k : b * k + b],
-            # where b is the dimension of box representation (4 or 5)
-            # Note that compared to Detectron1,
-            # we do not perform bounding box regression for background classes.
-            gt_class_cols = box_dim * fg_gt_classes[:, None] + torch.arange(box_dim, device=device)
-
-        loss_box_reg = smooth_l1_loss(
-            self.pred_proposal_deltas[fg_inds[:, None], gt_class_cols],
-            gt_proposal_deltas[fg_inds],
-            self.smooth_l1_beta,
-            reduction="sum",
-        )
-        # The loss is normalized using the total number of regions (R), not the number
-        # of foreground regions even though the box regression loss is only defined on
-        # foreground regions. Why? Because doing so gives equal training influence to
-        # each foreground example. To see how, consider two different minibatches:
-        #  (1) Contains a single foreground region
-        #  (2) Contains 100 foreground regions
-        # If we normalize by the number of foreground regions, the single example in
-        # minibatch (1) will be given 100 times as much influence as each foreground
-        # example in minibatch (2). Normalizing by the total number of regions, R,
-        # means that the single example in minibatch (1) and each of the 100 examples
-        # in minibatch (2) are given equal influence.
-        loss_box_reg = loss_box_reg / self.gt_classes.numel()
-        return loss_box_reg
-
-    def _predict_boxes(self):
-        """
-        Returns:
-            Tensor: A Tensors of predicted class-specific or class-agnostic boxes
-                for all images in a batch. Element i has shape (Ri, K * B) or (Ri, B), where Ri is
-                the number of predicted objects for image i and B is the box dimension (4 or 5)
-        """
-        num_pred = len(self.proposals)
-        B = self.proposals.tensor.shape[1]
-        K = self.pred_proposal_deltas.shape[1] // B
-        boxes = self.box2box_transform.apply_deltas(
-            self.pred_proposal_deltas.view(num_pred * K, B),
-            self.proposals.tensor.unsqueeze(1).expand(num_pred, K, B).reshape(-1, B),
-        )
-        return boxes.view(num_pred, K * B)
-
     """
     A subclass is expected to have the following methods because
     they are used to query information about the head predictions.0
@@ -253,36 +134,6 @@ class EQLFastRCNNOutputs(FastRCNNOutputs):
             "loss_box_reg": self.smooth_l1_loss(),
         }
 
-    def predict_boxes(self):
-        """
-        Returns:
-            list[Tensor]: A list of Tensors of predicted class-specific or class-agnostic boxes
-                for each image. Element i has shape (Ri, K * B) or (Ri, B), where Ri is
-                the number of predicted objects for image i and B is the box dimension (4 or 5)
-        """
-        return self._predict_boxes().split(self.num_preds_per_image, dim=0)
-
-    def predict_boxes_for_gt_classes(self):
-        """
-        Returns:
-            list[Tensor]: A list of Tensors of predicted boxes for GT classes in case of
-                class-specific box head. Element i of the list has shape (Ri, B), where Ri is
-                the number of predicted objects for image i and B is the box dimension (4 or 5)
-        """
-        predicted_boxes = self._predict_boxes()
-        B = self.proposals.tensor.shape[1]
-        # If the box head is class-agnostic, then the method is equivalent to `predicted_boxes`.
-        if predicted_boxes.shape[1] > B:
-            num_pred = len(self.proposals)
-            num_classes = predicted_boxes.shape[1] // B
-            # Some proposals are ignored or have a background class. Their gt_classes
-            # cannot be used as index.
-            gt_classes = torch.clamp(self.gt_classes, 0, num_classes - 1)
-            predicted_boxes = predicted_boxes.view(num_pred, num_classes, B)[
-                torch.arange(num_pred, dtype=torch.long, device=predicted_boxes.device), gt_classes
-            ]
-        return predicted_boxes.split(self.num_preds_per_image, dim=0)
-
     def predict_probs(self):
         """
         Returns:
@@ -297,21 +148,3 @@ class EQLFastRCNNOutputs(FastRCNNOutputs):
         probs = torch.cat([probs, dummy_probs], dim=1)
 
         return probs.split(self.num_preds_per_image, dim=0)
-
-    def inference(self, score_thresh, nms_thresh, topk_per_image):
-        """
-        Args:
-            score_thresh (float): same as fast_rcnn_inference.
-            nms_thresh (float): same as fast_rcnn_inference.
-            topk_per_image (int): same as fast_rcnn_inference.
-        Returns:
-            list[Instances]: same as fast_rcnn_inference.
-            list[Tensor]: same as fast_rcnn_inference.
-        """
-        boxes = self.predict_boxes()
-        scores = self.predict_probs()
-        image_shapes = self.image_shapes
-
-        return fast_rcnn_inference(
-            boxes, scores, image_shapes, score_thresh, nms_thresh, topk_per_image
-        )
